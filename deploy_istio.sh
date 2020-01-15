0c140dbfa0163a6e7d0f0d7988f9201f69cd8c3e
Ivan Varabyeu <vansolo@posteo.net>
2020-01-15 16:26:07 +0100

    added notes to deploy app

--- deploy_istio.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-ISTIO_VERSION=1.4.3
-curl -O https://github.com/istio/istio/releases/download/$ISTIO_VERSION/istio-$ISTIO_VERSION-linux.tar.gz
-curl -O https://github.com/istio/istio/releases/download/$ISTIO_VERSION/istio-$ISTIO_VERSION-linux.tar.gz.sha256
-ls -la istio-*
-echo "check data integrity"
-#export vars
-#kubectl label namespace default istio-injection=enabled
-#kubectl get namespace -L istio-injection

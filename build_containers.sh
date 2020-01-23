f412e572fd9d3c12044749b8e1dac3f1f6c4d754
Ivan Varabyeu <vansolo@posteo.net>
2020-01-23 18:04:03 +0100

    script to build containers

---
+++ build_containers.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+echo "building alerts v1"
+docker build -t alerts:v1 src/alerts
+
+echo "building cpanel v1"
+docker build -t cpanel:v1 src/cpanel
+
+echo "building section v1"
+docker build -t section:v1 src/section
+
+echo "building collector v1"
+docker build -t collector:v1 src/collector
+
+echo "check if all 4 containers built"
+docker images | grep 'alerts\|cpanel\|collector\|section'

65ce6daae73406ba3b760cc9d3571461c62c55ba
Ivan Varabyeu <vansolo@posteo.net>
2020-01-25 17:00:31 +0100

    working minikube install script

--- create_minikube_cluster.sh
+++ create_minikube_cluster.sh
@@ -1,29 +1,80 @@
-#!/bin/sh
+#!/usr/bin/env bash
+#
+# It is supposed, that minikube and curl are installed in your system
+# Create minikube VM and download kubectl of the same version
 
-MEMORY=16384
-CPU=4
-K8S_VERSION="v1.15.7"
+# to make script exit when a command fails
+set -o errexit
+# to exit when script tries to use undeclared variables
+set -o nounset
 
-# the name of VM is 'airport'
-# to avoid conflicts with possible local minikube installations
-echo "create minikube VM"
-#minikube start -p airport --vm-driver=virtualbox --memory=$MEMORY --cpus=$CPU --kubernetes-version=$K8S_VERSION
+# current dir
+__dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
-# install kubectl
-echo "download kubectl of the same version as K8S"
-curl -LO https://storage.googleapis.com/kubernetes-release/release/$K8S_VERSION/bin/linux/amd64/kubectl
-echo "make kubectl executable"
-# check if downloaded successful
-FILE="./kubectl"
-MIN_SIZE=9000000
-SIZE=$(wc -c <"$FILE")
-if [ "$SIZE" -ge "$MIN_SIZE" ]; then
-  chmod +x ./kubectl
-else
-  echo "Error: Kubectl file not found"
-  echo "Please download kubectl version $K8S_VERSION manually:"
-  echo "curl -LO https://storage.googleapis.com/kubernetes-release/release/v1.15.7/bin/linux/amd64/kubectl"
-  echo "Make it executable with:"
-  echo "chmod +x ./kubectl"
-fi
-# sudo mv ./kubectl /usr/bin/kubectl
+main() {
+  check_requirements
+
+  local memory=16384
+  local cpu=4
+  local k8s_version="v1.15.7"
+  local vm_name="airport"
+
+  if create_minikube_vm && install_kubectl; then
+    echo "proceed to next step from project README"
+    echo "SUCCESS: minikube VM and kubectl configured"
+  fi
+}
+
+create_minikube_vm() {
+  # the name of VM is 'airport'
+  # to avoid conflicts with possible local minikube installations
+  echo "creating minikube VM"
+  minikube start \
+    -p "${vm_name}" \
+    --vm-driver=virtualbox \
+    --memory="${memory}" \
+    --cpus="${cpu}" \
+    --kubernetes-version="${k8s_version}"
+}
+
+install_kubectl() {
+  # install kubectl
+  echo "download kubectl of the same version as K8S"
+  curl -LO https://storage.googleapis.com/kubernetes-release/release/"${k8s_version}"/bin/linux/amd64/kubectl
+  echo "kubectl downloaded to ${__dir}"
+
+  # check size of downloaded file
+  local file="./kubectl"
+  local min_size=30000000
+  local size
+  size=$(wc -c <"${file}")
+  if [ "${size}" -ge "${min_size}" ]; then
+    echo "make kubectl executable"
+    chmod +x ./kubectl
+    check_minikube_running
+  else
+    echo "Error: Kubectl file is not proper size"
+    echo "Please download kubectl version ${k8s_version} manually:"
+    echo "curl -LO https://storage.googleapis.com/kubernetes-release/release/v1.15.7/bin/linux/amd64/kubectl"
+    echo "to project location: ${__dir}"
+    echo "Make it executable with:"
+    echo "chmod +x ./kubectl"
+    echo "FAILED: Kubectl file is not proper size"
+    exit 1
+  fi
+}
+
+check_requirements() {
+  echo "check requirements"
+}
+
+check_minikube_running() {
+  echo "minikube status:"
+  minikube status -p "${vm_name}"
+  echo "kubectl version:"
+  ./kubectl version --client
+  echo "kubernetes is up and running:"
+  ./kubectl get namespaces
+}
+
+main

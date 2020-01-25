dca2003b072f4a031f9023d647c4fd4c5ccee0ad
Ivan Varabyeu <vansolo@posteo.net>
2020-01-25 22:55:17 +0100

    all dockerfiles updated

--- build_containers.sh
+++ build_containers.sh
@@ -1,18 +1,18 @@
 #!/usr/bin/env bash
 #
-#
+# build containers locally
 
 echo "building alerts v1"
-docker build -t alerts:v1 src/alerts
+docker build -t van15h/alerts:v1 src/alerts
 
 echo "building cpanel v1"
-docker build -t cpanel:v1 src/cpanel
+docker build -t van15h/cpanel:v1 src/cpanel
 
 echo "building section v1"
-docker build -t section:v1 src/section
+docker build -t van15h/section:v1 src/section
 
 echo "building collector v1"
-docker build -t collector:v1 src/collector
+docker build -t van15h/collector:v1 src/collector
 
 echo "check if all 4 containers built"
 docker images | grep 'alerts\|cpanel\|collector\|section'

291bd6eee2f2d9ea689aa34ef6b1c30d1ae6b98d
Ivan Varabyeu <vansolo@posteo.net>
2020-02-16 22:08:48 +0100

    refactor

--- src/cpanel/main.py
+++ src/cpanel/main.py
@@ -9,88 +9,103 @@ import time
 app = Flask(__name__)
 
 port = int(os.environ.get('PORT', 8080))
-bind_to = {'hostname': '0.0.0.0', 'port': port}
+bind_to = {'hostname': '0.0.0.0',
+           'port': port}
 
-dest_alerts = os.environ.get('URL_ALERTS', 'http://alerts:8080')
-dest_collector = os.environ.get('URL_COLLECTOR', 'http://collector:8080')
+dest_alerts = os.environ.get('URL_ALERTS',
+                             'http://alerts:8080')
+dest_collector = os.environ.get('URL_COLLECTOR',
+                                'http://collector:8080')
 # system configuration file
 # what camera belongs what section
-filename = os.environ.get('FILENAME', 'config.json')
+filename = os.environ.get('FILENAME',
+                          'config.json')
 data = {}  # data from config file
 sections = {'sections': []}  # all sections
 cameras = {'cameras': []}  # all cameras
 stats_analysis = {'persons': [
-        {
-            'age': '88-99',
-            'gender': 'female',
-            'event': 'entry',
-            'timestamp': '2010-10-14T11:19:18.039111Z'
-        },
-    ]}
+    {
+        'age': '88-99',
+        'gender': 'female',
+        'event': 'entry',
+        'timestamp': '2010-10-14T11:19:18.039111Z'
+    },
+]}
 stats_alert = {}
 
 
-@app.route('/', methods=['GET'])
-@app.route('/index', methods=['GET'])
+@app.route('/',
+           methods=['GET'])
+@app.route('/index',
+           methods=['GET'])
 def index():
     return render_template('index.html')
 
 
-@app.route('/status', methods=['GET'])
+@app.route('/status',
+           methods=['GET'])
 def show_status():
     return Response('CPanel : Online',
                     mimetype='text/plain',
                     status=200)
 
 
-@app.route('/analysis', methods=['GET'])
+@app.route('/analysis',
+           methods=['GET'])
 def get_analysis():
     return Response(json.dumps(stats_analysis, indent=2),
                     status=200)
 
 
-@app.route('/alert', methods=['GET'])
+@app.route('/alert',
+           methods=['GET'])
 def get_current_alert():
-    if bool(stats_alert):
-        del stats_alert['image']
-    return Response(json.dumps(stats_alert, indent=2), status=200)
+    return Response(json.dumps(stats_alert, indent=2),
+                    status=200)
 
 
-@app.route('/analysis', methods=['POST'])
+@app.route('/analysis',
+           methods=['POST'])
 def post_analysis():
     global stats_analysis
     if (request.is_json == True):
         app.logger.debug('got statistic from analysis')
-        app.logger.debug('analysis timestamp:' +
-                         str(request.json['timestamp']))
+        app.logger.debug('analysis timestamp:'
+                         + str(request.json['timestamp']))
         stats_analysis = request.json
     else:
         return Response('', status=400)
     return Response('', status=200)
 
 
-@app.route('/alert', methods=['POST'])
+@app.route('/alert',
+           methods=['POST'])
 def post_alert():
     global stats_alert
     if (request.is_json == True):
         app.logger.debug('got alert from face recognition')
-        app.logger.debug('alert timestamp: ' + request.json['timestamp'])
+        app.logger.debug('alert timestamp: '
+                         + request.json['timestamp'])
         stats_alert = request.json
     else:
         return Response('', status=400)
     return Response('', status=200)
 
 
-@app.route('/collector/status', methods=['GET'])
+@app.route('/collector/status',
+           methods=['GET'])
 def get_collector_status():
     """get status of collector"""
     status_url = dest_collector + '/status'
     response = requests.get(status_url)
-    app.logger.debug('forwarding GET to collector: ' + status_url)
-    return Response(response.text, status=response.status_code)
+    app.logger.debug('forwarding GET to collector: '
+                     + status_url)
+    return Response(response.text,
+                    status=response.status_code)
 
 
-@app.route('/config', methods=['PUT'])
+@app.route('/config',
+           methods=['PUT'])
 def reload_config():
     """reload config after changes made"""
     app.logger.debug('system config reloaded')
@@ -101,10 +116,12 @@ def reload_config():
     cameras = {'cameras': []}
     sections = {'sections': []}
     open_file()
-    return Response("Config reloaded successful", status=200)
+    return Response("Config reloaded successful",
+                    status=200)
 
 
-@app.route('/cameras', methods=['GET'])
+@app.route('/cameras',
+           methods=['GET'])
 def get_cameras():
     """list of all cameras in system"""
     temp = {'cameras': []}
@@ -114,49 +131,70 @@ def get_cameras():
         cam['type'] = c['type']
         cam['section'] = c['section']
         temp.get('cameras').append(cam)
-    return Response(json.dumps(temp, indent=2), status=200)
+    return Response(json.dumps(temp, indent=2),
+                    status=200)
 
 
-@app.route('/cameras/<id>/state', methods=['GET'])
+@app.route('/cameras/<id>/state',
+           methods=['GET'])
 def get_state(id):
     """state of camera #id"""
-    response = requests.get(get_cam_url(id)+r'/state')
-    app.logger.debug('camera url: ' + get_cam_url(id)+r'/state')
-    app.logger.debug('get state of camera agent: ' + str(id))
-    return Response(response.text, status=response.status_code)
+    response = requests.get(get_cam_url(id)
+                            + r'/state')
+    app.logger.debug('camera url: '
+                     + get_cam_url(id)
+                     + r'/state')
+    app.logger.debug('get state of camera agent: '
+                     + str(id))
+    return Response(response.text,
+                    status=response.status_code)
 
 
-@app.route('/cameras/<id>/stream', methods=['POST'])
+@app.route('/cameras/<id>/stream',
+           methods=['POST'])
 def start_stream(id):
     """start stream for camera #id"""
     if (request.is_json == True):
-        app.logger.debug(get_cam_url(id)+r'/stream')
-        response = requests.post(get_cam_url(
-            id)+r'/stream', json=request.json, params=request.args)
-        app.logger.debug('start camera agent: ' + str(id))
+        app.logger.debug(get_cam_url(id)
+                         + r'/stream')
+        response = requests.post(get_cam_url(id)
+                                 + r'/stream',
+                                 json=request.json,
+                                 params=request.args)
+        app.logger.debug('start camera agent: '
+                         + str(id))
     else:
-        return abort(400)
-    return Response(response.text, status=response.status_code)
+        return Response('', status=400)
+    return Response(response.text,
+                    status=response.status_code)
 
 
-@app.route('/cameras/<id>/frame', methods=['GET'])
+@app.route('/cameras/<id>/frame',
+           methods=['GET'])
 def get_frame(id):
     """get one frame from camera #id"""
-    response = requests.get(get_cam_url(id)+r'/frame')
-    app.logger.debug('frame from camera agent: ' + str(id))
-    return Response(response.text, status=response.status_code)
+    response = requests.get(get_cam_url(id)
+                            + r'/frame')
+    app.logger.debug('frame from camera agent: '
+                     + str(id))
+    return Response(response.text,
+                    status=response.status_code)
 
 
-@app.route('/sections/<id>/status', methods=['GET'])
+@app.route('/sections/<id>/status',
+           methods=['GET'])
 def get_section_status(id):
     """get status of section"""
     status_url = get_section_url(id) + '/status'
-    app.logger.debug('forwarding GET to section: ' + status_url)
+    app.logger.debug('forwarding GET to section: '
+                     + status_url)
     response = requests.get(status_url)
-    return Response(response.text, status=response.status_code)
+    return Response(response.text,
+                    status=response.status_code)
 
 
-@app.route('/sections', methods=['GET'])
+@app.route('/sections',
+           methods=['GET'])
 def get_sections():
     """list all sections in system from internal config file"""
     temp = {'sections': []}
@@ -165,75 +203,98 @@ def get_sections():
         cam['id'] = c['id']
         cam['description'] = c['description']
         temp.get('sections').append(cam)
-    return Response(json.dumps(temp, indent=2), status=200)
+    return Response(json.dumps(temp, indent=2),
+                    status=200)
 
 
-@app.route('/sections/<id>/persons', methods=['POST'])
+@app.route('/sections/<id>/persons',
+           methods=['POST'])
 def save_persons(id):
     """save analyzed persons in section #id"""
     if (request.is_json == True):
-        app.logger.debug('save persons in section: ' + str(id))
-        response = requests.post(get_section_url(
-            id)+r'/persons', json=request.json)
+        app.logger.debug('save persons in section: '
+                         + str(id))
+        response = requests.post(get_section_url(id)
+                                 + r'/persons',
+                                 json=request.json)
     else:
-        return abort(400)
-    return Response(response.text, status=response.status_code)
+        return Response('', status=400)
+    return Response(response.text,
+                    status=response.status_code)
 
 
-@app.route('/sections/<id>/persons', methods=['GET'])
+@app.route('/sections/<id>/persons',
+           methods=['GET'])
 def filter_persons(id):
     """filter persons in section #id by parameters"""
-    response = requests.get(get_section_url(
-        id)+r'/persons', params=request.args)
-    app.logger.debug('persons from section: ' + str(id))
-    return Response(response.text, status=response.status_code)
+    response = requests.get(get_section_url(id)
+                            + r'/persons',
+                            params=request.args)
+    app.logger.debug('persons from section: '
+                     + str(id))
+    return Response(response.text,
+                    status=response.status_code)
 
 
-@app.route('/alerts/', methods=['POST'])
+@app.route('/alerts/',
+           methods=['POST'])
 def save_alerts():
     """save new alert"""
     if (request.is_json == True):
         app.logger.debug('save alerts from recognition: ')
-        response = requests.post(dest_alerts + '/', json=request.json)
+        response = requests.post(dest_alerts + '/',
+                                 json=request.json)
     else:
-        return abort(400)
-    return Response(response.text, status=response.status_code)
+        return Response('', status=400)
+    return Response(response.text,
+                    status=response.status_code)
 
 
-@app.route('/alerts/', methods=['GET'])
+@app.route('/alerts/',
+           methods=['GET'])
 def filter_alerts():
     """list alerts by parameters"""
     request_url = dest_alerts + '/'
-    response = requests.get(request_url, params=request.args)
-    app.logger.debug('filter alerts: ' + request_url)
-    return Response(response.text, status=response.status_code)
+    response = requests.get(request_url,
+                            params=request.args)
+    app.logger.debug('filter alerts: '
+                     + request_url)
+    return Response(response.text,
+                    status=response.status_code)
 
 
-@app.route('/alerts/status', methods=['GET'])
+@app.route('/alerts/status',
+           methods=['GET'])
 def get_alerts_status():
     """get status of alerts"""
     status_url = dest_alerts + '/status'
     response = requests.get(status_url)
-    app.logger.debug('forwarding GET to alerts: ' + status_url)
-    return Response(response.text, status=response.status_code)
+    app.logger.debug('forwarding GET to alerts: '
+                     + status_url)
+    return Response(response.text,
+                    status=response.status_code)
 
 
-@app.route('/alerts/<id>', methods=['GET'])
+@app.route('/alerts/<id>',
+           methods=['GET'])
 def get_alert(id):
     """get alert by id"""
     app.logger.debug('get alert by id: ' + str(id))
     response = requests.get(dest_alerts + '/' + str(id))
     app.logger.debug('get alert by id: ' + str(id))
-    return Response(response.text, status=response.status_code)
+    return Response(response.text,
+                    status=response.status_code)
 
 
-@app.route('/alerts/<id>', methods=['DELETE'])
+@app.route('/alerts/<id>',
+           methods=['DELETE'])
 def delete_alert(id):
     """delete alert by id"""
     app.logger.debug(dest_alerts + '/' + str(id))
     response = requests.delete(dest_alerts + '/' + str(id))
     app.logger.debug('delete alert by id: ' + str(id))
-    return Response(response.text, status=response.status_code)
+    return Response(response.text,
+                    status=response.status_code)
 
 
 def get_cam_url(id):
@@ -257,8 +318,6 @@ def open_file():
     global data
     global cameras
     global sections
-    # global dest_alerts
-    # global dest_collector
     # open data file if exists
     file = pathlib.Path(filename)
     if file.exists():
@@ -278,11 +337,10 @@ def open_file():
         cameras['cameras'].append(cam)
     # read all sections from config
     sections['sections'] = data.get('sections')
-    # dest_alerts = data.get('alerts').get('url')
-    # dest_collector = data.get('collector').get('url')
 
 
-@app.route('/production', methods=['GET'])
+@app.route('/production',
+           methods=['GET'])
 def toggle():
     """start/stop all cameras to stream"""
     param = request.args
@@ -299,18 +357,24 @@ def toggle():
     if param.get('toggle') == 'on' or len(request.args) == 0:
         app.logger.debug('send on to camera agents: ')
         for id in range(len(cameras['cameras'])):
-            requests.post(get_cam_url(id+1)+r'/stream',
+            requests.post(get_cam_url(id+1)
+                          + r'/stream',
                           json=json_body[id])
         headers = [{'Access-Control-Allow-Origin', '*'}]
-        return Response('', status=200, headers=headers)
+        return Response('',
+                        status=200,
+                        headers=headers)
     elif param.get('toggle') == 'off':
         app.logger.debug('send off to camera agents: ')
         for id in range(len(cameras['cameras'])):
-            requests.post(get_cam_url(id+1)+r'/stream?toggle=off',
+            requests.post(get_cam_url(id+1)
+                          + r'/stream?toggle=off',
                           json=json_body[id])
         headers = [{'Access-Control-Allow-Origin', '*'}]
-        return Response('', status=200, headers=headers)
-    return abort(400)
+        return Response('',
+                        status=200,
+                        headers=headers)
+    return Response('', status=400)
 
 
 if __name__ == '__main__':

73c5e2a3e5f90acba1ef6478f505db6c997f6b70
Ivan Varabyeu <vansolo@posteo.net>
2020-02-29 21:59:30 +0100

    circuit breaker first try

--- load_fortio.sh
+++ load_fortio.sh
@@ -5,4 +5,4 @@
 FORTIO_POD=$(kubectl get pod | grep fortio | awk '{ print $1 }')
 echo "fortio pod: " "${FORTIO_POD}"
 echo "generating load to cpanel"
-kubectl exec -it "${FORTIO_POD}"  -c fortio /usr/bin/fortio -- load -c 2 -qps 0 -n 20 -loglevel Warning http://cpanel:8080/
+kubectl exec -it "${FORTIO_POD}"  -c fortio /usr/bin/fortio -- load -c 3 -qps 0 -n 20 -loglevel Warning http://cpanel:8080/
